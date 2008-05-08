6e2736db37889123c80689142850e748fd4a4070
pharaohgameengine <pharaohgameengine@555db735-7c4c-0410-acd0-0358cc0c1ac3>
2008-05-08 15:49:43 +0000

    Added a folder remotely

---
+++ PharaohGameEngine/PgePoint2D.cpp
@@ -0,0 +1,46 @@
+
+/*! $Id$
+ *  @file   PgePoint2D.h
+ *  @author Chad M. Draper
+ *  @date   May 7, 2007
+ *
+ */
+
+#include "PgePoint2D.h"
+#include "PgePoint3D.h"
+
+namespace PGE
+{
+//    ////////////////////////////////////////////////////////////////////////////
+//    // Point2D
+//    ////////////////////////////////////////////////////////////////////////////
+//    Point2D::Point2D( const Point2Df& pt )
+//        : Point2DTempl<Int>( static_cast<Int>( pt.x ), static_cast<Int>( pt.y ) )
+//    {
+//    }
+//
+//    Point2D::Point2D( const Point3D& pt )
+//        : Point2DTempl<Int>( pt.x, pt.y )
+//    {
+//    }
+//
+//    ////////////////////////////////////////////////////////////////////////////
+//    // Point2Df
+//    ////////////////////////////////////////////////////////////////////////////
+//    Point2Df::Point2Df( const Point2D& pt )
+//        : Point2DTempl<Real>( static_cast<Real>( pt.x ), static_cast<Real>( pt.y ) )
+//    {
+//    }
+//
+//    Point2Df::Point2Df( const Point3Df& pt )
+//        : Point2DTempl<Real>( pt.x, pt.y )
+//    {
+//    }
+//
+//    //IsEqual-------------------------------------------------------------------
+//    bool Point2Df::IsEqual( const Point2Df& p )
+//    {
+//        return ( Math::RealEqual( x, p.x ) && Math::RealEqual( y, p.y ) );
+//    }
+
+} // namespace PGE
---
+++ PharaohGameEngine/PgeTextureManager.cpp
@@ -0,0 +1,90 @@
+
+/*! $Id$
+ *  @file   PgeTextureManager.h
+ *  @author Chad M. Draper
+ *  @date   May 8, 2007
+ *
+ */
+
+#include "PgeTextureManager.h"
+
+namespace PGE
+{
+    ////////////////////////////////////////////////////////////////////////////
+    // TextureItem
+    ////////////////////////////////////////////////////////////////////////////
+
+    TextureItem::TextureItem( const String& imageFileName )
+        : mIsLoaded( false ),
+          mImageFileName( imageFileName )
+    {
+    }
+
+    //Load----------------------------------------------------------------------
+    void TextureItem::Load()
+    {
+        mIsLoaded = true;
+    }
+
+    //Unload--------------------------------------------------------------------
+    void TextureItem::Unload()
+    {
+        mIsLoaded = false;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // TextureManager
+    ////////////////////////////////////////////////////////////////////////////
+
+    // Instantiate the singleton instance
+    template<> TextureManager* Singleton< TextureManager >::mInstance = 0;
+    TextureManager& TextureManager::GetSingleton()
+    {
+        assert( mInstance );
+        return *mInstance;
+    }
+    TextureManager* TextureManager::GetSingletonPtr()
+    {
+        return mInstance;
+    }
+
+    TextureManager::TextureManager()
+    {
+    }
+
+    TextureManager::~TextureManager()
+    {
+        mTextureMap.clear();
+    }
+
+    //AddImage------------------------------------------------------------------
+    void TextureManager::AddImage( const String& imageFileName )
+    {
+        // Attempt to find the image in the map:
+        TextureIter iter = mTextureMap.find( imageFileName );
+
+        // Add the new item only if it is not found
+        if ( iter == mTextureMap.end() )
+        {
+            mTextureMap[ imageFileName ] = TextureItemPtr( new TextureItem( imageFileName ) );
+        }
+    }
+
+    //LoadImage-----------------------------------------------------------------
+    void TextureManager::LoadImage( const String& imageFileName )
+    {
+        // Make sure the image is in the map:
+        AddImage( imageFileName );
+
+        // Load the image:
+        TextureIter iter = mTextureMap.find( imageFileName );
+        if ( iter != mTextureMap.end() )
+            iter->second->Load();
+    }
+
+    //GetTextureItemPtr---------------------------------------------------------
+    TextureItem* TextureManager::GetTextureItemPtr() const
+    {
+    }
+
+} // namespace PGE
